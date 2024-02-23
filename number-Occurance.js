9d0b0682aae67b2926b2e5a22de5253768ff722f
tigersomu <68802131+tigersomu@users.noreply.github.com>
2024-02-23 17:48:06 +0530

    Update number-Occurance.js

--- number-Occurance.js
+++ number-Occurance.js
@@ -1,10 +1,16 @@
-const numbers = [2,4,5,2,1,2];
-let k= 2;
-// console.log(numbers.length);
-let noc=0;
-for(i=0;i<=numbers.length;i++){
-    if(k==numbers[i]){
-        noc=noc+1
+var k = 2;
+let noc = 0;
+function numOccurred() {
+    const numbers = [2, 4, 5, 2, 1, 2];
+    // var k = 2;
+    // console.log(numbers.length);
+    // let noc = 0;
+    for (i = 0; i <= numbers.length; i++) {
+        if (k == numbers[i]) {
+            noc = noc + 1
+            //console.log(noc);
+        }
     }
+    console.log("The number " + k + " is occurred " + noc + " times");
 }
-console.log("The number "+ k + " is occurred " + noc +" times");
+numOccurred();
